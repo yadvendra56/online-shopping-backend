1f3435410f1095b558cb8e110146d77f9bba963c
yadvendra56 <yadvendrakulkarni56@gmail.com>
2026-03-01 18:41:52 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from app.routers import products
+from app.routers import products, users, cart, orders
 
 app = FastAPI(title="Online Shopping API")
 
 app.include_router(products.router, prefix="/products", tags=["Products"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(cart.router, prefix="/cart", tags=["Cart"])
+app.include_router(orders.router, prefix="/orders", tags=["Orders"])
 
 @app.get("/")
 def root():
